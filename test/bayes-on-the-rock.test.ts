61cca20f126d74fc9bb1ac43da51c6fb0312b371
Raphael P <raphael@imagine-app.fr>
2019-02-13 15:30:11 +0100

    Initial commit

---
+++ test/bayes-on-the-rock.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from "../src/bayes-on-the-rock"
+
+/**
+ * Dummy test
+ */
+describe("Dummy test", () => {
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy()
+  })
+
+  it("DummyClass is instantiable", () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
