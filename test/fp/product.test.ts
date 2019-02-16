0a00d31d1e8d7237de259ba6a605cb466318c3ba
Raphael P <raphael@imagine-app.fr>
2019-02-16 22:52:42 +0100

    Basic implementation of product

---
+++ test/fp/product.test.ts
@@ -0,0 +1,23 @@
+import { Dice } from "../../src/ChoiceSet"
+import { product } from "../../src/fp/product"
+
+describe("Product test", () => {
+  it("Combine 2 Events", () => {
+    const event1 = { value: "toto", weight: 3 }
+    const event2 = { value: "tata", weight: 4 }
+
+    expect(product([event1, event2])).toEqual(
+      { value: ["toto", "tata"], weight: 12 }, 
+    )
+  })
+
+  it("Combine 3 Events", () => {
+    const event1 = { value: "toto", weight: 3 }
+    const event2 = { value: "tata", weight: 4 }
+    const event3 = { value: "tutu", weight: 5 }
+
+    expect(product([event1, event2, event3])).toEqual(
+      { value: ["toto", "tata", "tutu"], weight: 60 }, 
+    )
+  })
+})
