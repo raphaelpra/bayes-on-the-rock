25d9512fc67f2c2c9c72dda813c95fbac1cd3479
Raphael P <raphael@imagine-app.fr>
2019-02-16 23:11:40 +0100

    Styles

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import { tap } from "lodash/fp"
 
 export const arrayParametersToArray = (...args: any) => args
+
 export const logAndReturn = (prefix: string) =>
   tap((args: any) => console.log(prefix, args))
